673da830e520acd606073c44a66915d12e6268a2
Andrey <andbel2@bk.ru>
2026-08-20 00:40:08 +0300

    feat: added Log(message) to ILogger

--- lib/ILogger.hpp
+++ lib/ILogger.hpp
@@ -15,6 +15,8 @@ public:
 
     virtual ErrorCode Log(std::string_view message, ImportanceLevel level) = 0;
 
+    virtual ErrorCode Log(std::string_view message) = 0;
+
     virtual void SetDefaultImportanceLevel(ImportanceLevel level) = 0;
 
     virtual ~ILogger() = default;
